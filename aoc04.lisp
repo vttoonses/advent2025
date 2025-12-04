a7e84dd9964d18938b86a6870edc8859e4214395
Eugene Thompson <gene@thompson93.us>
2025-12-04 14:32:02 -0500

    day four complete (slight refactor)

--- aoc04.lisp
+++ aoc04.lisp
@@ -12,7 +12,7 @@
   (if (or (< row 0) (< col 0) (>= row rows) (>= col cols))
     (values nil nil)
     (let ((cell-val (char (elt diagram row) col)))
-      (values (or (char= val cell-val) (char= #\x cell-val)) t))))
+      (values (char= val cell-val) t))))
 
 (defun count-neighbor (diagram rows cols row col val)
   (multiple-value-bind (match valid) (check-cell diagram rows cols row col val)
@@ -28,7 +28,7 @@
     (list row (- col 1)) (list row (+ col 1))
     (list (+ row 1) (- col 1)) (list (+ row 1) col) (list (+ row 1) (+ col 1))))
 
-(defun count-accessable (diagram rows cols)
+(defun count-accessable (diagram rows cols clean)
   (let ((total 0))
     (loop for row from 0 below rows do
       (loop for col from 0 below cols do
@@ -36,20 +36,17 @@
           (let* ((neighbors (generate-neighbor-list row col)))
             (if (< (count-neighbors diagram rows cols #\@ neighbors) 4)
               (progn
-                (setf (char (elt diagram row) col) #\x)
+                (if clean (setf (char (elt diagram row) col) #\.))
                 (incf total)))))))
     total))
 
 (defun clean-warehouse (diagram rows cols)
-  (let ((pass-total (count-accessable diagram rows cols)))
+  (let ((pass-total (count-accessable diagram rows cols t)))
     (if (= pass-total 0) 0
-      (progn
-        (loop for row from 0 below rows do
-          (setf (elt diagram row) (substitute #\. #\x (elt diagram row))))
-        (+ pass-total (clean-warehouse diagram rows cols))))))
+        (+ pass-total (clean-warehouse diagram rows cols)))))
 
 (let* ((diagram (get-diagram "aoc04.txt")) (rows (length diagram)) (cols (length (elt diagram 0))))
-  (format t "Part 1 Total: ~a~%" (count-accessable diagram rows cols)))
+  (format t "Part 1 Total: ~a~%" (count-accessable diagram rows cols nil)))
 
 (let* ((diagram (get-diagram "aoc04.txt")) (rows (length diagram)) (cols (length (elt diagram 0))))
   (format t "Part 2 Total: ~a~%" (clean-warehouse diagram rows cols)))
