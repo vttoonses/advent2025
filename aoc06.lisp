c9d64b9c2bd4014a05f865f7af6c6eca22d664d3
Eugene Thompson <gene@thompson93.us>
2025-12-06 08:25:55 -0500

    day six part one ccomplete

---
+++ aoc06.lisp
@@ -0,0 +1,30 @@
+(defun tokenizer (string &key (delimiterp #'whitespace-p))
+    (loop :for beg = (position-if-not delimiterp string)
+        :then (position-if-not delimiterp string :start (1+ end))
+        :for end = (and beg (position-if delimiterp string :start beg))
+        :when beg :collect (subseq string beg end)
+        :while end))
+
+(defun whitespace-p (char)
+  (member char '(#\Space #\Tab #\Newline #\Return)))
+
+(defun add-ops (ops probs)
+  (if ops
+    (let ((op (car ops)) (prob (car probs)))
+      (cond
+        ((string= op "+") (setf op '+))
+        ((string= op "*") (setf op '*))
+        (t (setf op (parse-integer op))))
+      (cons (cons op prob) (add-ops (cdr ops) (cdr probs))))))
+
+(defun get-problems (filename)
+  (let ((problems '()))
+    (with-open-file (in filename :direction :input)
+      (when in
+        (loop for line = (read-line in nil :eof)
+          until (eq line :eof) do
+            (let ((ops (tokenizer line)))
+              (if problems
+                (setf problems (add-ops ops problems))
+                (setf problems (mapcar #'(lambda (o) (list (parse-integer o))) ops)))))))
+    (reduce #'+ (mapcar #'eval problems))))
