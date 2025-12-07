776f65d086a6e6cf7213a5c166205d80bbaddf13
Eugene Thompson <gene@thompson93.us>
2025-12-07 11:17:07 -0500

    day seven complete (refct part 1)

--- aoc07.lisp
+++ aoc07.lisp
@@ -4,18 +4,27 @@
       ((eq line :eof) 0)
       (t
         (loop for lcv from 0 below (length line) do
-          (if (and (char= #\^ (char line lcv)) (char= #\| (char beams lcv)))
+          (if (and (char= #\^ (char line lcv)) (> (elt beams lcv) 0))
             (progn
               (incf current-splits)
-              (cond
-                ((zerop lcv) (setf (char beams (+ 1 lcv)) #\|) (setf (char beams lcv) #\.))
-                ((= lcv (- (length line) 1)) (setf (char beams (- lcv 1)) #\|) (setf (char beams lcv) #\.))
-                (t (setf (char beams (+ 1 lcv)) #\|) (setf (char beams lcv) #\.) (setf (char beams (- lcv 1)) #\|))))))
+              (setf (elt beams (+ 1 lcv)) 1) (setf (elt beams lcv) 0) (setf (elt beams (- lcv 1)) 1))))
         (+ current-splits (count-splits data-file beams))))))
 
-(defun part-one (filename)
+(defun count-timelines (data-file beams)
+  (let ((line (read-line data-file nil :eof)))
+    (cond
+      ((eq line :eof) (reduce #'+ beams))
+      (t (loop for lcv from 0 below (length line) do
+            (if (and (char= (char line lcv) #\^) (> (elt beams lcv) 0))
+              (progn
+                (incf (elt beams (- lcv 1)) (elt beams lcv))
+                (incf (elt beams (+ lcv 1)) (elt beams lcv))
+                (setf (elt beams lcv) 0))))
+          (count-timelines data-file beams)))))
+
+(defun day07 (filename count-fn)
   (with-open-file (in filename :direction :input)
       (when in
-        (let* ((line (read-line in nil :eof)) (beams (make-string (length line) :initial-element #\.)))
-          (setf (char beams (position #\S line)) #\|)
-          (count-splits in beams)))))
+        (let* ((line (read-line in nil :eof)) (beams (make-array (length line) :initial-element 0)))
+          (setf (elt beams (position #\S line)) 1)
+          (funcall count-fn in beams)))))
